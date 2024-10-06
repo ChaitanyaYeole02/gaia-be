2169fcbee2d920a87a653837a12141c7e437a8ee
ChaitanyaYeole02 <cyeole@buffalo.edu>
2024-10-06 01:00:35 -0400

    file restructured

--- routes.py
+++ routes.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, jsonify
-from db import db  # Import the global `db` object
+import os
+import json
 
+import google.generativeai as genai
+
+from flask import Blueprint, jsonify, request
+from dotenv import load_dotenv
+
+from db import db
+
+load_dotenv()
 test = Blueprint('test', __name__)
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
 
 @test.route('/')
 def index():
@@ -10,8 +21,26 @@ def index():
 
 @test.route('/get_markers', methods=['GET'])
 def get_markers():
-    # Get the data from mongo
-    # jsonify and return it back
     return "fetched"
 
+
+@test.route('/get_difference', methods=['POST'])
+def get_difference():
+    try:
+        after_image = request.files['after_image']
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        before_image = genai.upload_file(request.files['before_image'])
+        after_image = genai.upload_file(path="test_image.jpg")
+        print(before_image, after_image)
+
+        # response = model.generate_content(
+        #     [before_image, after_image, "\n\n", "Compare these two images and highlight their differences."]
+        # )
+
+        # return response.text
+        return {}
+
+    except Exception as e:
+        print(f"An exception occurred: {e}")
+        return jsonify({"error": str(e)}), 500
  
--- run.py
+++ run.py
@@ -1,28 +1,26 @@
 import os 
 import configparser
-from flask import Flask 
-# from .db import init_db  
+
+from flask import Flask
+from dotenv import load_dotenv
+
+from db import init_db
 from routes import test
+
 
 def create_app():
     app = Flask(__name__)
-    config = configparser.ConfigParser()
-    config.read(os.path.abspath(os.path.join('config.ini')))
+    load_dotenv()
+    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
-    # Configure Flask MongoDB URI
-    app.config['MONGO_URI'] = "mongodb+srv://quynhho1601:dXWBa84LToNdQs8q@cluster0.rt183.mongodb.net/"
-
-    # Initialize additional configurations or extensions here
     with app.app_context():
-        app.register_blueprint(test)                                                                                        
+        app.register_blueprint(test)
+
+    init_db(app)
+
     return app
 
 
-
-
-#write a get api and fetch markers data from mongodb
-app = create_app()
-
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
-
